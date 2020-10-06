634b2a73fc1292a322981eaf86de3eefabb553ef
Andrew Heft <aheft@uoguelph.ca>
2020-10-06 17:54:33 -0400

    Update Player.java

--- src/main/java/rogue/Player.java
+++ src/main/java/rogue/Player.java
@@ -5,48 +5,40 @@ import java.awt.Point;
  * The player character
  */
 public class Player {
-
-
+    private String name;
+    private Point newXyLocation;
+    private Room newRoom;
 
     // Default constructor
     public Player() {
-
     }
-
 
     public Player(String name) {
-
+        this.name = name;
     }
-
 
     public String getName() {
-
-        return null;
+        return this.name;
     }
 
-
     public void setName(String newName) {
-
+        this.name = newName;
     }
 
     public Point getXyLocation() {
-        return null;
-
+        return newXyLocation;
     }
-
 
     public void setXyLocation(Point newXyLocation) {
-
+        this.newXyLocation = newXyLocation;
     }
-
 
     public Room getCurrentRoom() {
-        return null;
-
+        return this.newRoom;
     }
-
 
     public void setCurrentRoom(Room newRoom) {
-
+        this.newRoom = newRoom;
     }
 }
+
